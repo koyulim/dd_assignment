8c52a24b5d909988ea24beb628ef79e490b7db7e
고유림 <89690794+koyulim@users.noreply.github.com>
2025-03-14 17:35:12 +0900

    commit

--- JAVASCRIPT/task_5/script.js
+++ JAVASCRIPT/task_5/script.js
@@ -1,7 +1,3 @@
-// HTML 기초 학습 미션으로 만들었던 개인 소개 웹페이지에 div, p, button 요소를 자바스크립트로 선택하고, innerText나 style 속성을 사용하여 텍스트와 색상을 변경합니다. 
-// 예를 들어, 버튼을 클릭하면 문장의 색상이 변경되도록 이벤트 리스너를 추가합니다. 
-// 결과물로 웹 페이지의 브라우저 콘솔창에서 코드를 실행한 결과가 나오게 캡처하여 스크린샷을 제출합니다.
-
 var chack = true;
 var button = document.querySelector('.colorChange');
 var background = document.getElementById('blueChange');
