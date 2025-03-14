df903e29cbadf97cb8eef6263128da5ed60e6d1c
고유림 <89690794+koyulim@users.noreply.github.com>
2025-03-14 16:04:48 +0900

    change_title

--- ALGORITHM/basic_math/1. 운동 중독 플레이어/Main.java
+++ ALGORITHM/basic_math/1. 운동 중독 플레이어/Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-class Main {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
